f5e7f87d165fad99f7249707025e90ffe98a0845
prem-avalox <premavalox@gmail.com>
2024-08-15 20:30:39 -0500

    He finalizado el Script base y he llenado la base de datos

--- Script/DMExamScript.sql
+++ Script/DMExamScript.sql
@@ -1,1 +1,99 @@
 -- database: ../DataBase/DMDataBase.sqlite
+DROP TABLE IF EXISTS DMLocacion;
+
+DROP TABLE IF EXISTS DMAnimal;
+
+DROP TABLE IF EXISTS DMCatalogo;
+
+DROP TABLE IF EXISTS DMTipoCatalogo;
+
+CREATE TABLE
+    DMTipoCatalogo (
+        idTipoCatalogo INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombreTipoCatalogo VARCHAR(50) NOT NULL,
+        Estado VARCHAR(1) NOT NULL DEFAULT ('A'),
+        FechaCreacion DATETIME DEFAULT (datetime('now', 'localtime')),
+        FechaModifica DATETIME
+    );
+
+CREATE TABLE
+    DMCatalogo (
+        idCatalogo INTEGER PRIMARY KEY AUTOINCREMENT,
+        idTipo INTEGER NOT NULL REFERENCES DMTipoCatalogo (IdTipoCatalogo),
+        nombreCatalogo VARCHAR(50) NOT NULL,
+        Estado VARCHAR(1) NOT NULL DEFAULT ('A'),
+        FechaCreacion DATETIME DEFAULT (datetime('now', 'localtime')),
+        FechaModifica DATETIME
+    );
+
+CREATE TABLE
+    DMAnimal (
+        idAnimal INTEGER PRIMARY KEY AUTOINCREMENT,
+        idCatalogoSexo INTEGER NOT NULL REFERENCES DMCatalogo (idCatalogo),
+        idCatalogoClasificacion INTEGER NOT NULL REFERENCES DMCatalogo (idCatalogo),
+        idCatalogoGenoAlimento INTEGER NOT NULL REFERENCES DMCatalogo (idCatalogo),
+        Estado VARCHAR(1) NOT NULL DEFAULT ('A'),
+        FechaCreacion DATETIME DEFAULT (datetime('now', 'localtime')),
+        FechaModifica DATETIME
+    );
+
+CREATE TABLE
+    DMLocacion (
+        idLocacion INTEGER PRIMARY KEY AUTOINCREMENT,
+        Pais VARCHAR(20) NOT NULL,
+        Region VARCHAR(20) NOT NULL ,
+        Provincia VARCHAR(20) NOT NULL UNIQUE,
+        Estado VARCHAR(1) NOT NULL DEFAULT ('A'),
+        FechaCreacion DATETIME DEFAULT (datetime('now', 'localtime')),
+        FechaModifica DATETIME
+    );
+
+INSERT INTO DMTipoCatalogo
+(nombreTipoCatalogo )VALUES
+('Sexo'),
+('Clasificacion'),
+('GenoAlimento');
+INSERT INTO DMCatalogo
+(idTipo, nombreCatalogo )VALUES
+(1, 'Macho'),
+(1, 'Hembra'),
+(1, 'Asexual'),
+(2, 'Ave'),
+(2, 'Pez'),
+(2, 'Mamifero'),
+(3, 'AX'),
+(3, 'AXX'),
+(3, 'AXY');
+
+INSERT INTO DMLocacion
+(Pais, Region, Provincia)VALUES
+('Ecuador', 'Costa', 'Esmeraldas'),
+('Ecuador', 'Costa', 'Manabí'),
+('Ecuador', 'Costa', 'Guayas'),
+('Ecuador', 'Costa', 'Santa Elena'),
+('Ecuador', 'Costa', 'Los Ríos'),
+('Ecuador', 'Costa', 'El Oro'),
+('Ecuador', 'Costa', 'Santo Domingo de los Tsáchilas'),
+('Ecuador', 'Sierra', 'Carchi'),
+('Ecuador', 'Sierra', 'Imbabura'),
+('Ecuador', 'Sierra', 'Pichincha'),
+('Ecuador', 'Sierra', 'Cotopaxi'),
+('Ecuador', 'Sierra', 'Tungurahua'),
+('Ecuador', 'Sierra', 'Chimborazo'),
+('Ecuador', 'Sierra', 'Bolívar'),
+('Ecuador', 'Sierra', 'Cañar'),
+('Ecuador', 'Sierra', 'Azuay'),
+('Ecuador', 'Sierra', 'Loja'),
+('Ecuador', 'Oriente', 'Sucumbíos'),
+('Ecuador', 'Oriente', 'Napo'),
+('Ecuador', 'Oriente', 'Orellana'),
+('Ecuador', 'Oriente', 'Pastaza'),
+('Ecuador', 'Oriente', 'Morona Santiago'),
+('Ecuador', 'Oriente', 'Zamora Chinchipe'),
+('Ecuador', 'Insular', 'Galápagos');
+
+
+
+
+
+
