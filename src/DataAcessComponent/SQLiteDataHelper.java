d7736599e9c48c71a774caac188b39c68ef238ae
prem-avalox <premavalox@gmail.com>
2024-08-15 20:35:44 -0500

    He agregado mis paquetes de arquitectura y mi data helper

---
+++ src/DataAcessComponent/SQLiteDataHelper.java
@@ -0,0 +1,32 @@
+package DataAccess;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class SQLiteDataHelper {
+
+    private static String DBPathConnection = "jdbc:sqlite:DataBase/DataBase.sqlite";
+    private static Connection conn = null;
+
+    protected static synchronized Connection openConnection() throws Exception {
+        try {
+            if (conn == null) {
+                conn = DriverManager.getConnection(DBPathConnection);
+            }
+        } catch (SQLException e) {
+            throw e;
+        }
+        return conn;
+    }
+
+    protected static void closeConnection() throws Exception {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
