03b709385bf4a964ffb005e071cd8aa265b72346
prem-avalox <premavalox@gmail.com>
2024-08-16 09:15:24 -0500

    s file removed

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ import UserInterface.Forms.DMMainForm;
 public class App {
     public static void main(String[] args) throws Exception {
 
-       new DMMainForm("EcuFauna");
+       new DMMainForm("EcuFauna 2k24A");
         
         // // s.setIdSexo(3);
         // // s.setNombre("Hibrido");
